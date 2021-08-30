606315900f41d6891f6ff213fcd76c98b62440ff
Maitê <81782090+maitecr@users.noreply.github.com>
2021-08-30 12:40:34 -0300

    Create Sonic.java

---
+++ Sonic.java
@@ -0,0 +1,18 @@
+public class Sonic {
+
+	public static void main(String[] args) {
+		//Método Construtor
+		Personagem sonic = new Personagem();
+		
+		//Métodos
+		sonic.status();
+		sonic.perderEstamina();
+		sonic.ganharEstamina();
+		sonic.revigorar();
+		sonic.morrer();
+		sonic.coletarMoedas();
+		sonic.ganharVelocidade();
+		sonic.perderVelocidade();
+	}
+
+}
