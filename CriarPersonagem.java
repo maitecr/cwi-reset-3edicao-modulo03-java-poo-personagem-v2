84f4a49ea7e0ccf08bd19dba1ca2714d2cf262f9
Maitê <81782090+maitecr@users.noreply.github.com>
2021-08-31 14:43:54 -0300

    Create CriarPersonagem.java

---
+++ CriarPersonagem.java
@@ -0,0 +1,20 @@
+public class CriarPersonagens {
+
+	public static void main(String[] args) {
+		
+		Personagem p[] = new Personagem[2];
+		
+		p[0] = new Personagem("Sonic", 80, false);
+		p[1] = new Personagem("Snuckles", 50, true);
+		
+		p[0].status();
+		p[1].status();
+		
+		p[0].aumentarVelocidade();
+		p[1].aumentarVelocidade();
+		
+		p[0].status();
+		p[1].status();
+	}
+
+}
